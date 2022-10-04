fdc2c6f40ab2c81e44ea4e11f0e0c2111542ce98
Adnan <adnanhashmi1106@gmail.com>
2022-10-04 16:27:42 +0500

    Initial commit

---
+++ handyman_user_flutter/android/app/src/main/kotlin/com/adnanhashmi1106/multiservice/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.adnanhashmi1106.multiservice
+
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+class MainActivity: FlutterFragmentActivity() {
+}
---
+++ handyman_user_flutter/lib/utils/configs.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+const APP_NAME = 'Multi Service App';
+const APP_NAME_TAG_LINE = 'Online Booking App';
+const DEFAULT_LANGUAGE = 'en';
+
+// You can change this to your provider package name
+const PROVIDER_PACKAGE_NAME = 'com.adnanhashmi1106.providerapp';
+const IOS_LINK_FOR_PARTNER = "https://apps.apple.com/in/app/handyman-provider-app/id1596025324";
+
+var defaultPrimaryColor = Color(0xFF5F60B9);
+
+const DOMAIN_URL = '';
+const BASE_URL = '$DOMAIN_URL/api/';
+
+const ONESIGNAL_APP_ID = '';
+const ONESIGNAL_REST_KEY = "";
+const ONESIGNAL_CHANNEL_ID = "";
+
+const TERMS_CONDITION_URL = '';
+const PRIVACY_POLICY_URL = '';
+const HELP_SUPPORT_URL = '';
+
+
+const STRIPE_PAYMENT_PUBLISH_KEY = '';
+const STRIPE_MERCHANT_COUNTRY_CODE = 'PAK';
+const STRIPE_CURRENCY_CODE = 'PKR';
+
+DateTime todayDate = DateTime(2022, 10, 03);
